6e8a4bb909afcde4d8f2da195d5c181cac18669d
kirill <kirill.s.b.88@gmail.com>
2022-01-20 11:42:57 +0200

    yyy

---
+++ main.py
@@ -0,0 +1,30 @@
+inp_v1=input('value1 ')
+inp_v2=input('value2 ')
+#main(
+_res=None
+if inp_v1.isdigit() and inp_v2.isdigit():
+    operand=input('operand ')
+    inp_v1=int(inp_v1)
+    inp_v2=int(inp_v2)
+    if operand=='*':
+        _res=inp_v1*inp_v2
+        print(_res)
+    elif operand=='/':
+        if inp_v2==0:
+            print('error')
+            exit()
+            _res=inp_v1/inp_v2
+            print(_res)
+    elif operand=='+':
+        _res=inp_v1+inp_v2
+        print(_res)
+    elif operand =='-':
+        _res = inp_v1 -inp_v2
+        print(_res)
+else:
+    print('error')
+#)
+#inp_v1='1'
+#inp_v2='3'
+#operand='*'
+#main()
